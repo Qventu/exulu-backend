2b9a7862e343c02f97a59230a07d5197c056ac56
daniel.claessen <daniel.claessen@interlutions.de>
2025-08-21 15:18:26 +0200

    fix: function for default values in core schema and init-db

--- src/registry/utils/map-types.ts
+++ src/registry/utils/map-types.ts
@@ -2,57 +2,80 @@ import type { ExuluFieldTypes } from "@EXULU_TYPES/enums/field-types";
 
 export const mapType = (t, type: ExuluFieldTypes | "date" | "uuid" | "array", name: string, defaultValue?: any, unique?: boolean) => {
     if (type === "text" || type === "enum") {
-        t.text(name);
+        if (defaultValue) { 
+            t.text(name).defaultTo(defaultValue);
+         } else {
+            t.text(name);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "longText") {
-        t.text(name);
+        if (defaultValue) {
+            t.text(name).defaultTo(defaultValue);
+        } else {
+            t.text(name);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "shortText") {
-        t.string(name, 100);
+        if (defaultValue) {
+            t.string(name, 100).defaultTo(defaultValue);
+        } else {
+            t.string(name, 100);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "number") {
-        t.float(name);
+        if (defaultValue) {
+            t.float(name).defaultTo(defaultValue);
+        } else {
+            t.float(name);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "boolean") {
         t.boolean(name).defaultTo(defaultValue || false);
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "code") {
-        t.text(name);
+        if (defaultValue) {
+            t.text(name).defaultTo(defaultValue);
+        } else {
+            t.text(name);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "json") {
-        t.jsonb(name);
+        if (defaultValue) {
+            t.jsonb(name).defaultTo(defaultValue);
+        } else {
+            t.jsonb(name);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "date") {
-        t.timestamp(name);
+        if (defaultValue) {
+            t.timestamp(name).defaultTo(defaultValue);
+        } else {
+            t.timestamp(name);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     if (type === "uuid") {
-        t.uuid(name);
+        if (defaultValue) {
+            t.uuid(name).defaultTo(defaultValue);
+        } else {
+            t.uuid(name);
+        }
         if (unique) t.unique(name);
-        if (defaultValue) t.defaultTo(defaultValue);
         return;
     }
     throw new Error("Invalid type: " + type);
